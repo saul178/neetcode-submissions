8d3fc76846b7c95f1031223443bd765eea5d3127
saul178 <75957564+saul178@users.noreply.github.com>
2026-05-15 16:12:44 -0600

    Add: singlyLinkedList - submission-2

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-2.go
@@ -0,0 +1,78 @@
+type Node struct {
+    val int
+    next *Node
+}
+
+func NewNode(val int, nextNode *Node) *Node {
+    return &Node{
+        val: val,
+        next: nextNode,
+    }
+}
+type LinkedList struct {
+    head *Node
+    tail *Node
+
+}
+
+func NewLinkedList() *LinkedList {
+	head := NewNode(-1, nil)
+	return &LinkedList{
+		head: head,
+		tail: head,
+	}
+}
+
+func (ll *LinkedList) Get(index int) int {
+	curr := ll.head.next
+	i := 0
+	for curr != nil {
+		if i == index {
+			return curr.val
+		}
+		i++
+		curr = curr.next
+	}
+	return -1
+}
+
+func (ll *LinkedList) InsertHead(val int) {
+	newNode := NewNode(val, ll.head.next)
+	ll.head.next = newNode
+	if newNode.next == nil {
+		ll.tail = newNode
+	}
+}
+
+func (ll *LinkedList) InsertTail(val int) {
+	ll.tail.next = NewNode(val, nil)
+	ll.tail = ll.tail.next
+}
+
+func (ll *LinkedList) Remove(index int) bool {
+	i := 0
+	curr := ll.head
+	for i < index && curr != nil {
+		i++
+		curr = curr.next
+	}
+
+	if curr != nil && curr.next != nil {
+		if curr.next == ll.tail {
+			ll.tail = curr
+		}
+		curr.next = curr.next.next
+		return true
+	}
+	return false
+}
+
+func (ll *LinkedList) GetValues() []int {
+	curr := ll.head.next
+	res := []int{}
+	for curr != nil {
+		res = append(res, curr.val)
+		curr = curr.next
+	}
+	return res
+}
